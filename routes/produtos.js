378acff8dc9c3b52834732b0835df40e277de0ec
juliagascho <juliagascho@hotmail.com>
2023-04-28 11:41:11 -0300

    Put produtos finalizado

--- routes/produtos.js
+++ routes/produtos.js
@@ -81,6 +81,26 @@ router.get("/produtos/:id", async (req, res) => {
   }
 });
 
+//ROTA PARA ATUALIZAR UM PRODUTO
+router.put("/produto/:id", async (req, res) => {
+  const { nome, preco, descricao, desconto, dataDesconto, categoria } = req.body;
+  const { id } = req.params;
+
+  try {
+
+      const produto = await Produto.findOne({ where: { id } });
+
+      if (produto) {
+          await produto.update({ nome, preco, descricao, desconto, dataDesconto, categoria });
+          res.status(200).json({ message: "Produto editado." });
+      } else {
+          res.status(404).json({ message: "Produto não encontrado." });
+      }
+  } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Um erro aconteceu." });
+  }
+});
 
 
 module.exports = router;
