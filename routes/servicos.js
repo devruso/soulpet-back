9d6538b45b73cf25a0c2b7d9a55441434df667de
https://github.com/EuricoOliveira/html.git <https://github.com/EuricoOliveira/html.git>
2023-04-28 14:54:28 -0300

    Rotas Deletar servicos

--- routes/servicos.js
+++ routes/servicos.js
@@ -20,4 +20,32 @@ router.post("/servico", async (req, res) => {
     }
   });
 
+  router.delete("/servico/:id", async (req, res) => {
+    const { id } = req.params;
+    const servico = await Servico.findByPk(req.params.id);
+  
+    try {
+      if (servico) {
+        await servico.destroy();
+        res.status(200).json({ message: "Serviço removido." });
+      } else {
+        res.status(404).json({ message: "Serviço não encontrado." });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Um erro aconteceu." });
+    }
+  });
+  
+  router.delete("/servicos/all", async (req, res) => {
+    try {
+      await Servico.destroy({ where: {} });
+      res.status(200).json({ message: "Todos os serviços foram removidos." });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Um erro aconteceu." });
+    }
+  });
+  
+
   module.exports= router;
